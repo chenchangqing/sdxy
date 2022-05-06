fca6d0fcdface9312f99937cf99049d718df2f38
chenchangqing <chenchangqing@126.com>
2022-05-06 22:34:47 +0800

    no comment

--- gitbook.sh
+++ gitbook.sh
@@ -9,17 +9,17 @@ else
 	git push -u origin develop
 fi
 
-# git checkout master
-# git fetch origin
-# git rebase origin/develop
-# git push -u origin master
-# gitbook build
-# git checkout gh-pages
-# git pull origin gh-pages
-# rm -rf !(_book|.git|.gitignore|ignore|node_modules)
-# cp -r _book/* .
-# git add .
-# git commit -m 'build gh-pages'
-# git push -u origin gh-pages
-# rm -r -f _book
-# echo "提交OK"
+git checkout master
+git fetch origin
+git rebase origin/develop
+git push -u origin master
+gitbook build
+git checkout gh-pages
+git pull origin gh-pages
+rm -rf !(_book|.git|.gitignore|ignore|node_modules)
+cp -r _book/* .
+git add .
+git commit -m 'build gh-pages'
+git push -u origin gh-pages
+rm -r -f _book
+echo "提交OK"
